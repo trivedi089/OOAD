05f488793c645e91d382563f3331595663d31ace
Satya Trivedi <satyendra.trivedi@swiggy.in>
2023-04-16 12:50:31 +0530

    specifications based searching added

--- ricks-guitar-software/src/main/Main.java
+++ ricks-guitar-software/src/main/Main.java
@@ -7,9 +7,22 @@ public class Main {
 
         Inventory inventory = new Inventory();
 
+        //adding guitars to inventory
         inventory.addGuitars("V95693",1499.25,"Fender","Stratocastor","electric","Alder","Alder");
 
+        //searching based on serial number
         System.out.println(inventory.getGuitarOnSerialNumber("V95693"));
+
+        //Guitars which Erin Likes
+        Guitar whatErinLikes = new Guitar("",0,"fender","Stratocastor","electric","Alder","Alder");
+
+        Guitar guitar = inventory.search(whatErinLikes);
+        if(guitar!=null)
+        {
+            System.out.printf("Congratulations Erin! ",guitar);
+        }else{
+            System.out.println("Sorry Erin, we have nothing for you!");
+        }
 
     }
 }
